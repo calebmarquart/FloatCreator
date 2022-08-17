acf451a1e31cd0af25f647b9b955e92e25f4793b
Caleb Marquart <marquartcaleb@gmail.com>
2022-08-16 21:34:24 -0600

    Initial Commit

---
+++ Shared/FloatCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatCreatorApp.swift
+//  Shared
+//
+//  Created by Caleb Marquart on 2022-08-16.
+//
+
+import SwiftUI
+
+@main
+struct FloatCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
